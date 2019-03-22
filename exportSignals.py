c7279628279a220f75fbfc598eb5905a340fe562
daniel <daniel.hfc96@gmail.com>
2019-03-22 11:15:55 +0000

    Save time array as float32

--- exportSignals.py
+++ exportSignals.py
@@ -1,10 +1,12 @@
 import sys
+
 sys.path.append("D:/uni/daniel-tomografia")
 
-from isttok import myLib,baseline
-from isttok import signal 
+from isttok import myLib, baseline
+from isttok import signal
 from isttok.ISTTOKSignal import ISTTOKSignal
 import matplotlib
+
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 from scipy.ndimage import shift
@@ -15,7 +17,6 @@ from calibrationShots import shots, keys
 plt.close("all")
 
 plt.rcParams.update({'font.size': 18})
-
 
 channels = [
     "cal_top_01",
@@ -51,7 +52,7 @@ channels = [
     "cal_out_14",
     "cal_out_15",
     "cal_out_16",
-        ]
+]
 
 
 def export_signals(shot_id, plot=False):
@@ -85,24 +86,25 @@ def export_signals(shot_id, plot=False):
 
     signals_data = []
     signals_time = []
-    
-    shot=shots[shot_id]
-    
-    if plot: plt.figure(figsize=(20,12))
-    
+
+    shot = shots[shot_id]
+
+    if plot: plt.figure(figsize=(20, 12))
+
     for tag in channels:
-        
-        x=ISTTOKSignal(shot,tag=tag,uid=False,time_scale=1e-6,cache_dir='D:/uni/daniel-tomografia/local/shot-cache/')
-        detrend=baseline.baseline(x,deg=1)
-        data=x.values-detrend
-        time=x.times
-        
+
+        x = ISTTOKSignal(shot, tag=tag, uid=False, time_scale=1e-6,
+                         cache_dir='D:/uni/daniel-tomografia/local/shot-cache/')
+        detrend = baseline.baseline(x, deg=1)
+        data = x.values - detrend
+        time = x.times
+
         n = 10
         data = np.cumsum(data, axis=0)
-        data = (data[n:]-data[:-n])/n
+        data = (data[n:] - data[:-n]) / n
         data = data[::n]
         data = np.clip(data, 0., None)
-        time = time[n//2::n]
+        time = time[n // 2::n]
         time = time[:data.shape[0]]
         signals_data.append(data)
         signals_time.append(time)
@@ -112,28 +114,27 @@ def export_signals(shot_id, plot=False):
                 plt.title('signals (top camera)')
                 plt.xlabel('t (s)')
                 plt.legend()
-                plt.figure(figsize=(20,12))
+                plt.figure(figsize=(20, 12))
             if tag == channels[31]:
                 plt.title('signals (front camera)')
                 plt.xlabel('t (s)')
                 plt.legend()
             plt.show()
-    
-            
+
     # -------------------------------------------------------------------------
-    
-    signals_data = np.array(signals_data,dtype=np.float32).T
-    signals_time = np.array(signals_time,dtype=np.float32)
-    
+
+    signals_data = np.array(signals_data, dtype=np.float32).T
+    signals_time = np.array(signals_time, dtype=np.float32)
+
     print('signals_data:', signals_data.shape, signals_data.dtype)
     print('signals_time:', signals_time.shape, signals_time.dtype)
-    
+
     # -------------------------------------------------------------------------
-    
+
     fname = 'signals_data.npy'
     print('Writing:', fname)
     np.save(fname, signals_data)
-    
+
     fname = 'signals_time.npy'
     print('Writing:', fname)
     np.save(fname, signals_time)
@@ -178,7 +179,7 @@ def prepare_signals(shot_id, plot=False):
                          cache_dir='D:/uni/daniel-tomografia/local/shot-cache/')
         detrend = baseline.baseline(x, deg=1)
         data = x.values - detrend
-        time = x.times
+        time = np.array(x.times, dtype=np.float32)
 
         n = 10
         data = np.cumsum(data, axis=0)
@@ -218,7 +219,3 @@ def prepare_signals(shot_id, plot=False):
     # -------------------------------------------------------------------------
 
     return signals_time, signals_data
-
-
-
-
