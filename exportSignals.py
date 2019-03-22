5daf9b5e4b112bc5d7d0aad72fe2faba61e12662
daniel <daniel.hfc96@gmail.com>
2019-03-22 11:12:58 +0000

    add prepare signals function. Instead of saving signals to files, load from ISTTOK or local cache and return the signals ready for tomography

--- exportSignals.py
+++ exportSignals.py
@@ -57,6 +57,10 @@ channels = [
 def export_signals(shot_id, plot=False):
     """Prepare tomography signals for reconstruction. Work only with data from the discharge lamp experiments.
 
+    .. deprecated::
+        `export_signals` will be removed
+        `prepare_signals` should be used instead
+
     The reconstruction API relies on signals stored in .npy files. The signal preparation pipe-line follows.
     - Load the data from the ISTTOK data base or from the cache folder
     - Apply Signal processing techniques to the loaded data e.g. (detrending, baseline removal, filtering)
@@ -118,7 +122,7 @@ def export_signals(shot_id, plot=False):
             
     # -------------------------------------------------------------------------
     
-    signals_data = np.array(signals_data,dtype=np.float32)
+    signals_data = np.array(signals_data,dtype=np.float32).T
     signals_time = np.array(signals_time,dtype=np.float32)
     
     print('signals_data:', signals_data.shape, signals_data.dtype)
@@ -135,10 +139,86 @@ def export_signals(shot_id, plot=False):
     np.save(fname, signals_time)
 
     return signals_time, signals_data
-    
 
-if __name__ == '__main__':
-    export_signals(sys.argv[1], True)
+
+def prepare_signals(shot_id, plot=False):
+    """Prepare tomography signals for reconstruction. Works only with data from the discharge lamp experiments.
+
+    - Load the data from the ISTTOK data base or from the cache folder
+    - Apply Signal processing techniques to the loaded data e.g. (detrending, baseline removal, filtering)
+    - Return a time array with length T and a data array T x 32.
+    The data array can be directly served to the reconstruction classes
+
+    Parameters
+    ----------
+    shot_id: int
+        Shot number.
+    plot: Bool, optional
+        Show plots of the loaded data after processing
+
+    Returns
+    -------
+    signals_time: 1D array
+        Time array for the data of each tomography sensor
+    signals_data: N x 32 ND-array
+        Data from the sensors. Every row corresponds to a time value in `signals_time`.
+        Each column corresponds to one sensor.
+    """
+
+    signals_data = []
+
+    shot = shots[shot_id]
+
+    if plot:
+        plt.figure(figsize=(20, 12))
+
+    for tag in channels:
+
+        x = ISTTOKSignal(shot, tag=tag, uid=False, time_scale=1e-6,
+                         cache_dir='D:/uni/daniel-tomografia/local/shot-cache/')
+        detrend = baseline.baseline(x, deg=1)
+        data = x.values - detrend
+        time = x.times
+
+        n = 10
+        data = np.cumsum(data, axis=0)
+        data = (data[n:] - data[:-n]) / n
+        data = data[::n]
+        data = np.clip(data, 0., None)
+        time = time[n // 2::n]
+        time = time[:data.shape[0]]
+        signals_data.append(data)
+
+        try:
+            if not np.allclose(signals_time, time, 0.0, 1.e-8):
+                raise ValueError("tomography signals have different time axis")
+        except NameError:
+            signals_time = time
+
+        if plot:
+            plt.plot(time, data, label=tag)
+            if tag == channels[15]:
+                plt.title('signals (top camera)')
+                plt.xlabel('t (s)')
+                plt.legend()
+                plt.figure(figsize=(20, 12))
+            if tag == channels[31]:
+                plt.title('signals (front camera)')
+                plt.xlabel('t (s)')
+                plt.legend()
+            plt.show()
+
+    # -------------------------------------------------------------------------
+
+    signals_data = np.array(signals_data, dtype=np.float32).T
+
+    print('signals_data:', signals_data.shape, signals_data.dtype)
+    print('signals_time:', signals_time.shape, signals_time.dtype)
+
+    # -------------------------------------------------------------------------
+
+    return signals_time, signals_data
+
 
 
 
