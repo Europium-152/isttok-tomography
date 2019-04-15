f43a362b366d2f2b2bff7237a75d208bfc9cc18c
daniel <daniel.hfc96@gmail.com>
2019-04-15 01:15:36 +0100

    Add plotting routines for subplots

--- convergence/ReconstructSimulatedPhantom.py
+++ convergence/ReconstructSimulatedPhantom.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append("../")
 
 from projectionSelector import load_projection
@@ -10,16 +11,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import pearsonr as correlation
 
+# plot = False
 plot = True
 
 # Load the phantom ---------------------------------------------------------------------------------------
 
-phantom = produce_phantom(15., 15., 32., 1., 45, plot=True)
+phantom = produce_phantom(15., 15., 15., 1., 45, plot=False)
+phantom += produce_phantom(-15., 15., 15., 1., 45, plot=False)
+phantom += produce_phantom(5., -15., 15., 1., 45, plot=False)
+phantom += produce_phantom(0., 0., 65., 4., 45, plot=False)
+
+
+
+plt.figure()
+plt.imshow(phantom.reshape((45, 45)))
 
 # Load projection matrix ---------------------------------------------------------------------------------
 
-projections = load_projection("complex-view-cone.npy")
-projections = projections
+projections = load_projection("complex-view-cone-45.npy")
 
 # Simulate the signal created by the phantom -------------------------------------------------------------
 
@@ -29,46 +38,109 @@ signals = np.dot(projections.reshape((projections.shape[0], -1)), phantom)
 
 mfi = MFI(projections, width=200., height=200., mask_radius=85.)
 
+# Plot projections ---------------------------------------------------------------------------------------
+
+# for i in (0, 16, 32):
+#
+#     rows = 4
+#     cols = 4
+#
+#     fig, axes = plt.subplots(nrows=rows, ncols=cols)
+#
+#     for ax, p in zip(axes.flat, projections[i:i + 16]):
+#         ax.set_aspect('equal', 'datalim')
+#         im = ax.pcolormesh(mfi.x_array_plot, mfi.y_array_plot, p, vmin=None, vmax=None)
+#         circle = plt.Circle((0., 0.), 85., color='w', fill=False)
+#         ax.add_artist(circle)
+#
+#     fig.subplots_adjust(right=0.8)
+#     cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+#     fig.colorbar(im, cax=cbar_ax)
+#
+#     plt.show()
+
 # Run reconstruction -------------------------------------------------------------------------------------
 
-# alpha_1 = 5e-8
-# alpha_2 = 5e-8
-# alpha_3 = 1
-# alpha_4 = 0
-#
-# g_list, first_g = mfi.reconstruction_gpu(signals=signals,
-#                                      stop_criteria=0,
-#                                      alpha_1=alpha_1,
-#                                      alpha_2=alpha_2,
-#                                      alpha_3=alpha_3,
-#                                      alpha_4=alpha_4,
-#                                      max_iterations=10)
+alpha_1 = 10. ** -7
+alpha_2 = 10. ** -7
+alpha_3 = 10. ** 0
+alpha_4 = 0
+
+g_list = mfi.reconstruction_gpu(signals=signals,
+                                stop_criteria=0,
+                                alpha_1=alpha_1,
+                                alpha_2=alpha_2,
+                                alpha_3=alpha_3,
+                                alpha_4=alpha_4,
+                                max_iterations=19,
+                                iterations=True,
+                                verbose=True,
+                                guess=None)
 
 # Adaptive regularization constant -------------------------------------------------------------------
 
-alpha_3 = 1.
-alpha_4 = 0.
+# alpha_3 = 1.
+# alpha_4 = 0.
+#
+#
+# def comparison(g):
+#     return np.sum(np.abs(g.flatten()-phantom))
+#
+#
+# g_list, first_g, alpha_1 = mfi.tomogram(signals=signals,
+#                                         stop_criteria=0.,
+#                                         comparison=comparison,
+#                                         alpha_3=alpha_3,
+#                                         alpha_4=alpha_4,
+#                                         inner_max_iterations=8,
+#                                         outer_max_iterations=8)
 
-
-def comparison(g):
-    return np.sum(np.abs(g.flatten()-phantom))
-
-
-g_list, first_g, alpha_1 = mfi.tomogram(signals=signals,
-                                        stop_criteria=0.,
-                                        comparison=comparison,
-                                        alpha_3=alpha_3,
-                                        alpha_4=alpha_4,
-                                        inner_max_iterations=8,
-                                        outer_max_iterations=8)
+# Brute force search for good alpha ---------------------------------------------------------------------
+#
+# alphas = np.linspace(-9.0, -8.5, 10)
+# costs = []
+#
+#
+# def comparison(g):
+#     return np.sum(np.abs(g.flatten() - phantom))
+#
+#
+# for alpha in alphas:
+#
+#     alpha_1 = 10.**alpha
+#     alpha_2 = 10.**alpha
+#     alpha_3 = 1
+#     alpha_4 = 0
+#
+#     g_list, first_g = mfi.reconstruction_gpu(signals=signals,
+#                                              stop_criteria=0,
+#                                              alpha_1=alpha_1,
+#                                              alpha_2=alpha_2,
+#                                              alpha_3=alpha_3,
+#                                              alpha_4=alpha_4,
+#                                              max_iterations=10,
+#                                              verbose=True)
+#
+#     costs.append(np.log(comparison((g_list[-1]))))
+#
+# plt.figure()
+# plt.plot(alphas, costs)
+# # plt.xlim((-9.5, -7))
+# plt.show()
 
 #########################################################################
 #                                                                       #
 #                                PLOTTING                               #
 #                                                                       #
 #########################################################################
+
 if plot:
-    for G in g_list:
+
+    rows = 4
+    cols = 5
+
+    fig, axes = plt.subplots(nrows=rows, ncols=cols)
+    for ax, G in zip(axes.flat, g_list):
         # centroid = center_of_mass(G)
         # print ('centroid index : (%.2f, %.2f)' % (centroid[1], centroid[0]))
         # center_y = n_rows * res[1]/2. - centroid[0] * res[1]
@@ -83,6 +155,20 @@ if plot:
         #
         # print('maximum coords: (%.2f, %.2f)' % (max_x, max_y))
 
+        ax.set_aspect('equal', 'datalim')
+        im = ax.pcolormesh(mfi.x_array_plot, mfi.y_array_plot, G, vmin=None, vmax=None)
+        # ax.title(r"$\alpha$ = %f" % alpha_1)
+        circle = plt.Circle((0., 0.), 85., color='w', fill=False)
+        ax.add_artist(circle)
+
+    fig.subplots_adjust(right=0.8)
+    cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+    fig.colorbar(im, cax=cbar_ax)
+
+    plt.show()
+
+    for G in g_list[-2:]:
+
         plt.figure()
         plt.axes().set_aspect('equal', 'datalim')
         plt.pcolormesh(mfi.x_array_plot, mfi.y_array_plot, G, vmin=None, vmax=None)
@@ -95,5 +181,5 @@ if plot:
         plt.gca().add_artist(circle)
         # plt.savefig("phantom-reconstructions/phantom-%d-reconstruction.png" % phantom_number)
 
-plt.show()
-
+    plt.show()
+# plt.show()
