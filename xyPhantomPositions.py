8fc83ef57c6df2cf5261ed4769a4985b1caba8fd
daniel <daniel.hfc96@gmail.com>
2019-04-23 23:47:14 +0100

    Export all phantoms

--- xyPhantomPositions.py
+++ xyPhantomPositions.py
@@ -3,10 +3,12 @@ Create an emissivity Profile for the experimental phantoms
 """
 
 
-from .calibrationShots import keys
+from calibrationShots import keys
 import numpy as np
 from scipy.integrate import dblquad
 import matplotlib.pyplot as plt
+import matplotlib.style as mplstyle
+mplstyle.use('ggplot')
 
 
 # Define an analytical formula for the emissivity. Gaussian emissivity
@@ -33,8 +35,8 @@ for key in keys:
 
 # Discretization Parameters ---------------------------------------------------
 
-n_rows = 45  # y-axis number of pixels
-n_cols = 45  # x-axis number of pixels
+n_rows = 80  # y-axis number of pixels
+n_cols = 80  # x-axis number of pixels
 
 x_min = -100.
 x_max = +100.
@@ -51,38 +53,48 @@ res_y = (y_max - y_min) / n_rows
 
 G = np.zeros((n_rows, n_cols))  # G matrix
 
-index = 2  # Phantom to compute the G profile
+index = 5  # Phantom to compute the G profile
 
-# Emissivity parameters ------------------------------------------------------------
-mu_x, mu_y = xy_positions[index]  # phantom coordinates
-fwhm = 4.  # Use full width half maximum as the source diameter (4mm)
-area = 1.  # Value of the gaussian integral
+for index in np.arange(57):
 
-for i in range(n_rows):
-    for j in range(n_cols):
-        # Four corners of the pixel
-        x1 = x_min + j * res_x
-        x2 = x1 + res_x
-        y2 = y_max - i * res_y
-        y1 = y2 - res_y
+    G = np.zeros((n_rows, n_cols))  # G matrix
 
-        x = (x1 + x2) / 2.
-        y = (y1 + y2) / 2.
+    # Emissivity parameters ------------------------------------------------------------
+    mu_x, mu_y = xy_positions[index]  # phantom coordinates
+    fwhm = 4.  # Use full width half maximum as the source diameter (4mm)
+    area = 1.  # Value of the gaussian integral
 
-        if ((x - mu_x) ** 2 + (y - mu_y) ** 2) < (5 * fwhm) ** 2:
-            # The value at each pixel is the average value inside (integral divided by area)
-            G[i, j], _ = dblquad(em, y1, y2, lambda _: x1, lambda _: x2, args=(mu_x, mu_y, area, fwhm))
-            G[i, j] /= res_x * res_y
+    for i in range(n_rows):
+        for j in range(n_cols):
+            # Four corners of the pixel
+            x1 = x_min + j * res_x
+            x2 = x1 + res_x
+            y2 = y_max - i * res_y
+            y1 = y2 - res_y
 
-# x and y arrays for plotting purposes. Coordinates represent the top left corner of each pixel
-x_array_plot = (np.arange(n_cols + 1) - n_cols / 2.) * res_x
-y_array_plot = (n_rows / 2. - np.arange(n_rows + 1)) * res_y
+            x = (x1 + x2) / 2.
+            y = (y1 + y2) / 2.
 
-plt.figure()
-plt.pcolormesh(x_array_plot, y_array_plot, G, vmin=None, vmax=None)
-plt.colorbar()
-plt.show()
+            if ((x - mu_x) ** 2 + (y - mu_y) ** 2) < (5 * fwhm) ** 2:
+                # The value at each pixel is the average value inside (integral divided by area)
+                G[i, j], _ = dblquad(em, y1, y2, lambda _: x1, lambda _: x2, args=(mu_x, mu_y, area, fwhm))
+                G[i, j] /= res_x * res_y
 
-# Save the phantom emissivity in a file in vector form -------------------------------------------------------------
+    # x and y arrays for plotting purposes. Coordinates represent the top left corner of each pixel
+    x_array_plot = (np.arange(n_cols + 1) - n_cols / 2.) * res_x
+    y_array_plot = (n_rows / 2. - np.arange(n_rows + 1)) * res_y
 
-np.save("phantoms/Phantom-%d.npy" % index, G.flatten())
+    # plt.figure()
+    # plt.pcolormesh(x_array_plot, y_array_plot, G, vmin=None, vmax=None)
+    # plt.axis('scaled')
+    # circle = plt.Circle((0., 0.), 85., color='w', fill=False)
+    # plt.gca().add_artist(circle)
+    # cbar = plt.colorbar()
+    # cbar.set_label("\nIntensity (arb. units)")
+    # plt.xlabel("R (mm)")
+    # plt.ylabel("z (mm)")
+    # plt.show()
+
+    # Save the phantom emissivity in a file in vector form -------------------------------------------------------------
+
+    np.save("phantoms/Phantom-%d.npy" % index, G.flatten())
